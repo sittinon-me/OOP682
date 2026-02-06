116100251c6abe999c5d136672c4388c8f843ec4
sittinon-me <sittinon.me.68@ubu.ac.th>
2026-02-06 17:26:48 +0700

    week10

---
+++ fastapi-lab/app/repositories.py
@@ -0,0 +1,79 @@
+from abc import ABC, abstractmethod
+from typing import List ,Optional
+from .models import Task, TaskCreate
+
+
+class ITaskRepository(ABC):
+    
+    @abstractmethod
+    def get_all(self) -> List[Task]:
+        pass
+
+    @abstractmethod
+    def create(self, task: TaskCreate) -> Task:
+        pass
+        
+    @abstractmethod
+    def get_by_id(self, task_id: int) -> Optional[Task]:
+        pass
+    
+    @abstractmethod
+    def update_task_complete(self, task_id:int) -> Optional[Task]:
+        pass
+    
+    # @abstractmethod
+    # def get_by_title(self,titleone:Task) ->Task:
+    #     pass
+    
+class InMemoryTaskRepository(ITaskRepository):
+    def __init__(self):
+        self.tasks = []
+        self.current_id = 1
+
+    def get_all(self) -> List[Task]:
+        return self.tasks
+
+    def create(self, task_in: TaskCreate) -> Task:
+        task = Task(
+            id=self.current_id,
+            **task_in.dict()
+        )
+        self.tasks.append(task)
+        self.current_id += 1
+        return task
+
+    def get_by_id(self, task_id: int) -> Optional[Task]:
+        for task in self.tasks:
+            if task.id == task_id:
+                return task
+        return None
+
+    
+from sqlalchemy.orm import Session
+from . import models_orm  # ต้องสร้าง SQLAlchemy Model แยก
+
+class SqlTaskRepository(ITaskRepository):
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_all(self) -> List[Task]:
+        return self.db.query(models_orm.Task).all()
+
+    def create(self, task_in: TaskCreate) -> Task:
+        db_task = models_orm.Task(**task_in.dict())
+        self.db.add(db_task)
+        self.db.commit()
+        self.db.refresh(db_task)
+        return db_task
+    
+    def update_task_complete(self, task_id:int) -> Optional[Task]:
+        db_task = self.get_by_id(task_id)
+        if db_task:
+            db_task.completed = True
+            self.db.commit()
+            self.db.refresh(db_task)
+        return db_task
+    
+    def get_by_id(self, task_id: int):
+        return self.db.query(models_orm.Task).filter(models_orm.Task.id == task_id).first()
+    
---
+++ fastapi-lab/app/services.py
@@ -0,0 +1,17 @@
+from .repositories import ITaskRepository
+from .models import TaskCreate
+
+class TaskService:
+    def __init__(self, repo: ITaskRepository):
+        self.repo = repo
+
+    def get_tasks(self):
+        return self.repo.get_all()
+
+    def create_task(self, task_in: TaskCreate):
+        # Business logic could go here
+        return self.repo.create(task_in)
+    
+    def mark_task_complete(self, task_id: int):
+        return self.repo.update_task_complete(task_id)
+    
